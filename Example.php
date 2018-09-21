1403d1467d31bf91360991a2125bc4d7771259c2
Rafał Więcek <gitzzz14@gmail.com>
2018-09-21 14:31:12 +0200

    Update Example

--- Example.php
+++ Example.php
@@ -3,17 +3,22 @@ include( 'SimPay.php' );
 
 $simPay = new SimPay();
 
+//Ustawianie klucza api dostepnego w panelu usługi
 $simPay -> setApiKey( '<klucz api>' );
 
+//Parsowanie danych otrzymanych POST do danych zawartych w klasie i możliwych do pobrania poprzez metody
 $smsObject = $simPay -> parseSMS( $_POST );
 
+//Sprawdzenie czy przy parsowaniu wystąpił błąd
 if( $simPay -> isError() ){
-	//Wystapił błąd podczas przetwarzania sms'a
-
+	//Wystapił błąd podczas przetwarzania sms'a , wyświetlenie typu błędu
 	exit( $simPay -> getErrorText() );
 }
 
-//Generowanie kodu
+//Pobranie różnych części SMS'a np. SIM.TEST zwróci nam tablicę [ 'SIM' , 'TEST' ] lub SIM.TEST.TRESC zwróci nam [ 'SIM' , 'TEST' , 'TRESC' ] itp. itd.
+$arrayPieces = $simPay -> getPieces();
+
+//Generowanie kodu do zwrotu
 $smsCode = $simPay -> generateCode();
 
 //Zapisywanie wygenerowanego kodu w bazie
@@ -24,9 +29,9 @@ $stmt -> bindValue( ':amount' , $smsObject -> getValue(), PDO::PARAM_STR );
 	 
 $stmt -> execute();
 
-//Generowanie XML'a do wysyłki ( nie można używać poiskich znakó w treści sms'a )
+//Generowanie XML'a do wysyłki ( nie można używać polskich znaków treści sms'a )
 $return = $simPay -> generateXml( 'Twoj kod doladowania to ' . $smsCode );
 
-//Odpowiedź do serwerów simpay z wyugenerowanym XML'em
+//Odpowiedź do serwerów simpay z wygenerowanym XML'em
 echo $return;
 ?>
