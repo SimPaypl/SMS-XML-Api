c661f3344d12507146bf38cd13f6a3da3b22f332
Rafał Więcek <gitzzz14@gmail.com>
2020-03-18 03:53:25 +0000

    PSR-2 Update

--- examples/Example.php
+++ examples/Example.php
@@ -1,36 +1,36 @@
 <?php
-include( 'SimPay.php' );
+include('SimPay.php');
 
 //Ustawienia MYSQL
 $host = 'localhost';
-$port = '3306'; 
+$port = '3306';
 $username = 'username';
 $password = 'password';
 $database = 'database';
 $pdoObject = null;
 
 //Laczenie do bazy danych
-try{
-	$pdoObject = new PDO('mysql:host='.$host.';dbname='.$database.';port='.$port, $username, $password , array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4'"));
-	$pdoObject->query('SET NAMES utf8mb4');
-	$pdoObject->query('SET CHARACTER SET utf8mb4');
-	$pdoObject->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch( PDOException $e ){
-	exit();
+try {
+    $pdoObject = new PDO('mysql:host='.$host.';dbname='.$database.';port='.$port, $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4'"));
+    $pdoObject->query('SET NAMES utf8mb4');
+    $pdoObject->query('SET CHARACTER SET utf8mb4');
+    $pdoObject->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    exit();
 }
 
 $simPay = new SimPay();
 
 //Ustawianie klucza api dostepnego w panelu usługi
-$simPay -> setApiKey( '<klucz api>' );
+$simPay -> setApiKey('<klucz api>');
 
 //Parsowanie danych otrzymanych POST do danych zawartych w klasie i możliwych do pobrania poprzez metody
-$smsObject = $simPay -> parseSMS( $_POST );
+$smsObject = $simPay -> parseSMS($_POST);
 
 //Sprawdzenie czy przy parsowaniu wystąpił błąd
-if( $simPay -> isError() ){
-	//Wystapił błąd podczas przetwarzania sms'a , wyświetlenie typu błędu
-	exit( $simPay -> getErrorText() );
+if ($simPay -> isError()) {
+    //Wystapił błąd podczas przetwarzania sms'a , wyświetlenie typu błędu
+    exit($simPay -> getErrorText());
 }
 
 //Pobranie różnych części SMS'a np. SIM.TEST zwróci nam tablicę [ 'SIM' , 'TEST' ] lub SIM.TEST.TRESC zwróci nam [ 'SIM' , 'TEST' , 'TRESC' ] itp. itd.
@@ -40,16 +40,15 @@ $arrayPieces = $smsObject -> getPieces();
 $smsCode = $simPay -> generateCode();
 
 //Zapisywanie wygenerowanego kodu w bazie
-$stmt = $pdoObject -> prepare('INSERT INTO `generate_codes` ( `code` , `amount` ) VALUES ( :code , :amount )' );
+$stmt = $pdoObject -> prepare('INSERT INTO `generate_codes` ( `code` , `amount` ) VALUES ( :code , :amount )');
 
-$stmt -> bindValue( ':code' , $smsCode, PDO::PARAM_STR );
-$stmt -> bindValue( ':amount' , $smsObject -> getValue(), PDO::PARAM_STR );
-	 
+$stmt -> bindValue(':code', $smsCode, PDO::PARAM_STR);
+$stmt -> bindValue(':amount', $smsObject -> getValue(), PDO::PARAM_STR);
+     
 $stmt -> execute();
 
 //Generowanie XML'a do wysyłki ( nie można używać polskich znaków treści sms'a )
-$return = $simPay -> generateXml( 'Twoj kod doladowania to ' . $smsCode );
+$return = $simPay -> generateXml('Twoj kod doladowania to ' . $smsCode);
 
 //Odpowiedź do serwerów simpay z wygenerowanym XML'em
 echo $return;
-?>
